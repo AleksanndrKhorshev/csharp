bd109b27ab955e6848b1113fdfe1f70028e7b28e
Aleksandr Khorshev <ya.balkonprofi@yandex.ru>
2022-10-07 00:37:55 +0300

    qwe

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число a");
+int a = Convert.ToInt32 (ConsoleReadline ());
+Console.WriteLine("Введите число b");
+int b = Convert.ToInt32 (ConsoleReadline ());
+    int max = a
+    if (a > b)
+    {
+        Console.WriteLine(Число a больше) 
+    }
+    else
+    {
+        Console.WriteLine(Число b больше)
+    }
